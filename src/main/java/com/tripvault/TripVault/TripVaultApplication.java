5cd261ba82107cbb3157e0f93623383f17ca814e
Rohit <rohitsinghchandel420@gmail.com>
2026-02-13 22:29:56 +0530

    Project creation with first commit

---
+++ src/main/java/com/tripvault/TripVault/TripVaultApplication.java
@@ -0,0 +1,13 @@
+package com.tripvault.TripVault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tripvault/TripVault/TripVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tripvault.TripVault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
