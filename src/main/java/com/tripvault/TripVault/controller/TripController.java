4b072b2caa31fb1c4e6b0a246e674969be57228d
Rohit <rohitsinghchandel420@gmail.com>
2026-02-15 17:07:41 +0530

    Trip controller and service created

--- src/main/java/com/tripvault/TripVault/controller/TripController.java
+++ src/main/java/com/tripvault/TripVault/controller/TripController.java
@@ -1,7 +1,21 @@
 package com.tripvault.TripVault.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.tripvault.TripVault.model.Trip;
+import com.tripvault.TripVault.service.TripService;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/trips")
 public class TripController {
+
+    private final TripService tripService;
+
+    public TripController(TripService tripService){
+        this.tripService=tripService;
+    }
+
+    @PostMapping("/{userId}")
+    public Trip savedTrip(@PathVariable Long userId, @RequestBody Trip trip){
+        return tripService.createTrip(userId,trip);
+    }
 }
--- src/main/java/com/tripvault/TripVault/service/TripService.java
+++ src/main/java/com/tripvault/TripVault/service/TripService.java
@@ -1,7 +1,45 @@
 package com.tripvault.TripVault.service;
 
+import com.tripvault.TripVault.model.Trip;
+import com.tripvault.TripVault.model.TripMember;
+import com.tripvault.TripVault.model.TripRole;
+import com.tripvault.TripVault.model.User;
+import com.tripvault.TripVault.repository.TripMemberRepository;
+import com.tripvault.TripVault.repository.TripRepository;
+import com.tripvault.TripVault.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TripService {
+
+    private final TripRepository tripRepository;
+    private final UserRepository userRepository;
+    private final TripMemberRepository tripMemberRepository;
+
+    public TripService(TripRepository tripRepository,UserRepository userRepository,TripMemberRepository tripMemberRepository){
+        this.tripRepository=tripRepository;
+        this.userRepository=userRepository;
+        this.tripMemberRepository=tripMemberRepository;
+    }
+
+
+    public Trip createTrip(Long userId,Trip trip){
+        User user=userRepository.findById(userId).orElseThrow(()->new RuntimeException("No User Found"));
+
+        trip.setCreatedBy(user);
+
+        Trip savedTrip=tripRepository.save(trip);
+
+        TripMember owner=new TripMember();
+        owner.setTrip(savedTrip);
+        owner.setUser(user);
+        owner.setRole(TripRole.OWNER);
+        owner.setAllocatedBytes(0L);
+        owner.setUsedBytes(0L);
+
+        tripMemberRepository.save(owner);
+
+        return savedTrip;
+    }
+
 }
